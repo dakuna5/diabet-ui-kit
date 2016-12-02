24ec09621e76c4926d58e2e51aa01f999194c395
alexey <macbook@mac-mini-alexey>
2016-12-03 00:04:50 +0200

    Initial commit

---
+++ diabet/APIphoneSeVC.swift
@@ -0,0 +1,80 @@
+//
+//  APIphoneSeVC.swift
+//  diabet
+//
+//  Created by alexeyweb11@gmail.com on 02/12/2016, using AnimaApp.com, under MIT license.
+//  Copyright © 2016 Company Name. All rights reserved.
+//
+
+import UIKit
+
+public class APIphoneSeVC : ANViewController {
+    
+    @IBOutlet weak var list: UIView?
+    @IBOutlet weak var list9: UIView?
+    @IBOutlet weak var list9Line: UIView?
+    @IBOutlet weak var list9Erick: UIImageView?
+    @IBOutlet weak var list9Roy: UIImageView?
+    @IBOutlet weak var list8: UIView?
+    @IBOutlet weak var list8Line: UIView?
+    @IBOutlet weak var list8Jane: UIImageView?
+    @IBOutlet weak var list8Fletcher: UIImageView?
+    @IBOutlet weak var list7: UIView?
+    @IBOutlet weak var list7Line: UIView?
+    @IBOutlet weak var list7Pauline: UIImageView?
+    @IBOutlet weak var list7Simmons: UIImageView?
+    @IBOutlet weak var list6: UIView?
+    @IBOutlet weak var list6Line: UIView?
+    @IBOutlet weak var list6Ryan: UIImageView?
+    @IBOutlet weak var list6Phillips: UIImageView?
+    @IBOutlet weak var list5: UIView?
+    @IBOutlet weak var list5Line: UIView?
+    @IBOutlet weak var list5Chuck: UIImageView?
+    @IBOutlet weak var list5Willis: UIImageView?
+    @IBOutlet weak var list4: UIView?
+    @IBOutlet weak var list4Line: UIView?
+    @IBOutlet weak var list4Maria: UIImageView?
+    @IBOutlet weak var list4Smith: UIImageView?
+    @IBOutlet weak var list3: UIView?
+    @IBOutlet weak var list3Line: UIView?
+    @IBOutlet weak var list3Kim: UIImageView?
+    @IBOutlet weak var list3Hopkins: UIImageView?
+    @IBOutlet weak var list2: UIView?
+    @IBOutlet weak var list2Line: UIView?
+    @IBOutlet weak var list2Jack: UIImageView?
+    @IBOutlet weak var list2Milton: UIImageView?
+    @IBOutlet weak var list1: UIView?
+    @IBOutlet weak var list1Line: UIView?
+    @IBOutlet weak var list1Bill: UIImageView?
+    @IBOutlet weak var list1Watson: UIImageView?
+    @IBOutlet weak var toolBarsPhone: UIView?
+    @IBOutlet weak var toolBarsPhoneBackground: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhone: UIView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneVoicemail: UIView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneVoicemailShape: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneVoicemailVoicemail: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneVoicemailVoicemailIcon: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneKeypad: UIView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneKeypadShape: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneKeypadKeypad: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneKeypadKeypadIcon: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneContacts: UIView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneContactsShape: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneContactsContactsIcon: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneContactsContacts: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneRecents: UIView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneRecentsShape: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneRecentsRecents: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneRecentsRecentsIcon: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneFavourites: UIView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneFavouritesShape: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneFavouritesFavourites: UIImageView?
+    @IBOutlet weak var toolBarsPhoneToolBarsPhoneFavouritesFavouritesIcon: UIImageView?
+    @IBOutlet weak var divider: UIView?
+    @IBOutlet weak var dividerBackground: UIView?
+    @IBOutlet weak var dividerA: UIImageView?
+    @IBOutlet weak var topBar: UIImageView?
+
+    
+
+}
